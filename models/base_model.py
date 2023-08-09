d8f95a050912b39009797ec2186fce6150ef06ad
chimachike1@gmail.com <chimachike1@gmail.com>
2023-08-09 11:30:13 -0700

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,8 @@ class BaseModel():
         self.updated_at = datetime.now()
         if len(kwargs) != 0:
             for k, v in kwargs.items():
-                if k in ['created_at, updated_at']:
-                    self.__dict__[k] = datetime.strptime(v, '%y-%m-%dT%H:&M:%S.%f')
+                if k in ['created_at', 'updated_at']:
+                    self.__dict__[k] = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
                 else:
                     self.__dict__[k] = v
         else:
